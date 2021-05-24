bb23c80e86d2ca7f11354b85c4e86d0189b64d53
Gralhix <74664918+Gralhix@users.noreply.github.com>
2021-05-24 09:50:46 +0100

    Add files via upload

---
+++ pawpatrollist.py
@@ -0,0 +1,6 @@
+#Playing with lists, for loops and the len function
+
+pawpatrol = ["Marshall", "Zuma", "Rubble", "Chase", "Rocky", "Skye"]
+for i in range(len(pawpatrol)):
+    print("Pup " + str(i+1) + " in the Paw Patrol line up is: " + pawpatrol[i])
+    
